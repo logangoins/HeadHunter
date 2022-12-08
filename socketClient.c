bfc53e3d8849c853646002aeef237682523e6a10
Logan Goins <55106700+Lionskey@users.noreply.github.com>
2022-12-08 15:00:10 -0600

    Add files via upload

--- socketClient.c
+++ socketClient.c
@@ -12,10 +12,11 @@
 //Note the comments for variables in server code
 int main(){
 
+  char message[100];
   struct sockaddr_in serv; 
   int fd; 
   int conn; 
-  char message[100] = ""; 
+  int valread;
 
   fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -25,17 +26,24 @@ int main(){
   inet_pton(AF_INET, "127.0.0.1", &serv.sin_addr); //Binds client to localhost
 
   connect(fd, (struct sockaddr *)&serv, sizeof(serv)); //connects client to server
-
-  while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
-    int pid;
-    if((pid = fork()) == 0){
-      while (recv(conn, message, 100, 0) > 0) {
-        printf("Message Received: %s\n", message);
-        strcpy(message, "");
-      }
-      exit(0);
-    }
+  printf("Burger\n");
+  conn = accept(fd, (struct sockaddr *)NULL, NULL);
+  printf("Conneatataysdjf\n");
+  while(1){
+    fflush(stdout);
+    //while (recv(conn, &message, 100, 0) > 0) {
+    valread = read(fd, message, 100);
+    if(valread > 0){
+      printf("gotted\n");
+      printf("Message Received: %s\n", message);
+    }  
+    //strcpy(message, "");
+    //}
   }
+    
+
+  printf("Burger");
+  //}
 
   return 0;
 }
--- socketServer.c
+++ socketServer.c
@@ -10,6 +10,7 @@ struct sockaddr_in serv; //Main socket variable
 int fd; //socket identifier (file descripter)
 int conn; //connection identifier (connection file descripter)
 char message[100] = ""; //array to store messages
+int valread;
 
 int main(){
   
@@ -29,19 +30,17 @@ int main(){
   bind(fd, (struct sockaddr *)&serv, sizeof(serv)); //Assigns address to the socket
 
   listen(fd, 5); //Listen for client connections. Maximum 5 connections
-
-
-
   while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
       int pid;
       if((pid = fork()) == 0){
 
         printf("Enter a message: ");
-
+        
         fgets(message, 100, stdin);
-
+        printf("Message: %s\n", message);
+        fflush(stdout);
         send(fd, message, strlen(message), 0);
-
+        printf("Hehe - michael jackson\n");
         while (recv(conn, message, 100, 0) > 0) {
           printf("Message Received: %s\n", message);
           strcpy(message, "");
