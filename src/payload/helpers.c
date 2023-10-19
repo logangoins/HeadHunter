4614e9ac4bac1f3adcf7607ae65d37c39a20e7bb
Logan Goins <LoganGoins@protonmail.com>
2023-10-19 14:00:33 -0500

    Added strstartswith function to the payload helpers file

--- src/payload/helpers.c
+++ src/payload/helpers.c
@@ -4,15 +4,19 @@
 #include <stdlib.h>
 
 char* XOR(char* data, char* key, int datalen, int keylen) {
-
     char* output = (char*)malloc(sizeof(char) * datalen);
-    
     for (int i = 0; i < datalen; ++i){
 	output[i] = data[i] ^ key[i % keylen];
     }
-
     return output;    
 
 }
 
+int str_starts_with(char *a, char*b){
+    for(int i = 0; i < strlen(b); i++){
+        if (a[i] != b[i])
+            return 0;
+    }
+    return 1;
+}
 
