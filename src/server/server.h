b3b6576f5df9451315df0efd16655b9d3d42905d
Logan Goins <55106700+Lionskey@users.noreply.github.com>
2023-10-22 16:52:13 -0500

    Revert "Fix memory leaks, reformat project with clang-format"

--- src/server/server.h
+++ src/server/server.h
@@ -7,15 +7,13 @@
 #define PROMPT "headhunter/> "
 
 fd_set readfds;
-int master_socket, new_socket, max_clients = MAX_CLIENTS,
-                               client_socket[MAX_CLIENTS], activity, i, sd,
-                               victim_count;
+int master_socket, new_socket, max_clients = MAX_CLIENTS, client_socket[MAX_CLIENTS], activity, i, sd, victim_count;
 int max_sd;
 struct sockaddr_in cli;
 socklen_t len;
 
-// Declaration of struct "args" as the pthread_create function preferably takes
-// a struct as a param This is the struct for the Writer thread
+// Declaration of struct "args" as the pthread_create function preferably takes a struct as a param
+// This is the struct for the Writer thread
 typedef struct args
 {
     int src;
